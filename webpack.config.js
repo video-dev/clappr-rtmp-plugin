652890eac5020b3d5ef32ec864577651c7b0c75c
Germano Fronza <germano@eventials.com>
2015-11-07 15:58:58 -0200

    Build minified JS

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,20 @@
-var path = require('path');
-var TransferWebpackPlugin = require('transfer-webpack-plugin');
+var path = require('path'),
+    webpack = require('webpack'),
+    TransferWebpackPlugin = require('transfer-webpack-plugin'),
+    minimize = process.argv.indexOf('--no-minimize') === -1 ? true : false,
+    plugins = [new TransferWebpackPlugin([
+        { from: 'public/', to: 'assets/' }
+    ])],
+    filename = 'rtmp.js';
+
+if (minimize) {
+    plugins.push(new webpack.optimize.UglifyJsPlugin({minimize: true}));
+    filename = 'rtmp.min.js';
+}
 
 module.exports = {
     entry: path.resolve(__dirname, 'index.js'),
+    devtool: minimize ? "source-map" : "",
     externals: {
         clappr: 'Clappr',
     },
@@ -21,14 +33,10 @@ module.exports = {
     resolve: {
         extensions: ['', '.js'],
     },
-    plugins: [
-        new TransferWebpackPlugin([
-            { from: 'public/', to: 'assets/' }
-        ])
-    ],
+    plugins: plugins,
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'rtmp.js',
+        filename: filename,
         library: 'RTMP',
         libraryTarget: 'umd',
     },
